3f473d5483e336cb335e2dc833b6d82a229b25a0
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-03-06 16:09:59 +0700

    Code ui Register/Login UI

--- src/layouts/RegisterLayout/RegisterLayout.tsx
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Footer from 'src/components/Footer'
+import RegisterHeader from 'src/components/RegisterHeader'
 
-const RegisterLayout = () => {
-  return <div></div>
+interface Props {
+  children?: React.ReactNode
+}
+
+const RegisterLayout = ({ children }: Props) => {
+  return (
+    <div>
+      <RegisterHeader />
+      {children}
+      <Footer />
+    </div>
+  )
 }
 
 export default RegisterLayout
