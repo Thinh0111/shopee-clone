ea31b2f21b6966484065e914e4328f68b7d08c61
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-03-14 14:15:04 +0700

    update InputNumber && QuantityController vs localState component

--- src/components/InputNumber/InputNumber.tsx
+++ src/components/InputNumber/InputNumber.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState } from 'react'
 import { InputHTMLAttributes } from 'react'
 
 export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -14,24 +14,34 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     classNameInput = 'w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
     classNameError = 'mt-1 min-h-[1rem] text-sm text-red-600',
     onChange,
+    value = '',
     ...rest
   },
   ref
 ) {
+  // Trường hợp người dùng ko truyền onChange hay value thì input number nhập được chữ như này không hay mong muốn dù người dùng ko có truyền vào onChange hay value vào thì nó vẫn hoạt động đúng những gì mình set up logic trong đấy
+  // Trường hợp người dùng ko truyền onChange hay value vào thì mình sẽ sử dụng component InputNumber như sau sẽ tạo 1 state cục bộ
+  // Nếu người dùng truyền value vào thì lấy giá trị value trên này làm giá trị khởi tạo.(giá trị khởi tạo này chỉ hiệu nghiệm 1 lần redner đầu tiên thôi, nếu value thay đổi nó sẽ ko làm giá trị khởi tạo này thay đổi)
+  const [localValue, setLocalValue] = useState<string>(value as string)
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
 
     // reges kiểm tra số
-    if ((/^\d*$/.test(value) || value === '') && onChange) {
+    if (/^\d*$/.test(value) || value === '') {
       // Khi nhập số thì onChange mới chạy, còn nhập text thì onChange sẽ không chạy
       // bên này xuất ra event bên nhận sẽ nhận event, bên này xuất ra value bên nhận sẽ nhận value
-      onChange(e)
+      // Thực thi onchange callback từ bên ngoài truyền vào props
+      onChange && onChange(e)
+
+      // Cập nhật localValue  state
+      setLocalValue(value)
     }
   }
   return (
     <>
       <div className={className}>
-        <input className={classNameInput} {...rest} onChange={handleChange} ref={ref} />
+        <input className={classNameInput} {...rest} onChange={handleChange} ref={ref} value={value || localValue} />
         <div className={classNameError}>{errorMessage}</div>
       </div>
     </>
--- src/components/QuantityController/QuantityController.tsx
+++ src/components/QuantityController/QuantityController.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputNumber, { InputNumberProps } from '../InputNumber'
 
 interface Props extends InputNumberProps {
@@ -18,6 +18,8 @@ const QuantityController = ({
   value,
   ...rest
 }: Props) => {
+  const [localValue, setLocalValue] = useState<number>(Number(value || 0))
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let _value = Number(e.target.value)
 
@@ -31,24 +33,30 @@ const QuantityController = ({
 
     // Nếu có onType thì mình mới gọi và truyền value tránh lỗi
     onType && onType(_value)
+
+    setLocalValue(_value)
   }
 
   const increase = () => {
-    let _value = Number(value) + 1
+    let _value = Number(value || localValue) + 1
     if (max !== undefined && _value > max) {
       _value = max
     }
 
     onIncrease && onIncrease(_value)
+
+    setLocalValue(_value)
   }
 
   const decrease = () => {
-    let _value = Number(value) - 1
+    let _value = Number(value || localValue) - 1
     if (_value < 1) {
       _value = 1
     }
 
     onDecrease && onDecrease(_value)
+
+    setLocalValue(_value)
   }
 
   return (
@@ -73,7 +81,7 @@ const QuantityController = ({
         classNameError='hidden'
         classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
         onChange={handleChange}
-        value={value}
+        value={value || localValue}
         {...rest}
       />
       <button
